6ae1087775c67943eb363ca7417063738b0e499e
Alisson Perez <alissonperez@gmail.com>
2016-07-14 11:47:05 -0300

    Add employee and department models

--- hr/admin.py
+++ hr/admin.py
@@ -1,3 +1,13 @@
 from django.contrib import admin
+from hr import models
 
-# Register your models here.
+class DepartmentAdmin(admin.ModelAdmin):
+    pass
+
+
+class EmployeeAdmin(admin.ModelAdmin):
+    pass
+
+
+admin.site.register(models.Department, DepartmentAdmin)
+admin.site.register(models.Employee, EmployeeAdmin)
--- hr/models.py
+++ hr/models.py
@@ -1,3 +1,17 @@
 from django.db import models
 
-# Create your models here.
+
+class Department(models.Model):
+    name = models.CharField(max_length=30, unique=True)
+
+    def __str__(self):
+        return self.name
+
+
+class Employee(models.Model):
+    name = models.CharField(max_length=50)
+    email = models.EmailField(unique=True)
+    department = models.ForeignKey(Department)
+
+    def __str__(self):
+        return self.name
