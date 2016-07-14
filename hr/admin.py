1971e5510e7faf416a7c44fe7996c2e028d8c15e
Alisson Perez <alissonperez@gmail.com>
2016-07-14 14:33:26 -0300

    Improve Employees list on admin screen

--- hr/admin.py
+++ hr/admin.py
@@ -1,12 +1,16 @@
 from django.contrib import admin
 from hr import models
 
+
 class DepartmentAdmin(admin.ModelAdmin):
     pass
 
 
 class EmployeeAdmin(admin.ModelAdmin):
-    pass
+    list_display = ('name', 'department')
+
+    list_filter = ('department',)
+    search_fields = ('name',)
 
 
 admin.site.register(models.Department, DepartmentAdmin)
